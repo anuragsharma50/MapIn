abf2a6eea46e320f9cf47acd1d85f22b495ae114
Anurag Sharma <59228106+anuragsharma50@users.noreply.github.com>
2020-12-20 22:07:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function renderPlaces(places) {
        let model = document.createElement('a-entity');
        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
        model.setAttribute('gltf-model', './assets/avengers_logo/scene.gltf');
-       model.setAttribute('rotation', '0 0 0');
+       model.setAttribute('rotation', '0 90 0');
        model.setAttribute('animation-mixer', '');
        model.setAttribute('scale', '2 2 2');
 

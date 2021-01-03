19777e1f24b8a43fd9d79dd25c8390c14dcee7a3
Anurag Sharma <59228106+anuragsharma50@users.noreply.github.com>
2021-01-03 22:23:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function renderPlaces(places) {
         model.setAttribute('gltf-model', './assets/jumpboost_arrow/scene.gltf');
         model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.5 0.5 0.5');
+        model.setAttribute('scale', '0.01 0.01 0.01');
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))

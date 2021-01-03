92a08f31f80466838bda5dded9787fa898766983
Anurag Sharma <59228106+anuragsharma50@users.noreply.github.com>
2021-01-03 21:40:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function renderPlaces(places) {
 
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', 'https://github.com/anuragsharma50/marker/blob/main/pokemon_ball/scene.gltf');
+        model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
         model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', '0.5 0.5 0.5');
